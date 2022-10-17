33c4fb08cf8bea417fe3af86ede5b4d1ece73062
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-17 18:08:02 +0530

    half css done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,26 @@ const Home = () => {
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
-        <img src="/code-sync.png" alt="hi" />
+        <img className = "homePageLogo" src="/code-sync.png" alt="hi" />
         <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
         <div className="inputGroup">
-            <input type="text" className = "inputbox" placeholder = "ROOM ID" />
-            <input type="text" className = "inputbox" placeholder = "USERNAME" />
-            <button className="btn joinBtn">Join</button>
-            <span className="createInfo">
-                If you dont have an invite then create &nbsp;
-                <a href="" className="createNewBtn"> new room</a>
-            </span>
+          <input type="text" className="inputbox" placeholder="ROOM ID" />
+          <input type="text" className="inputbox" placeholder="USERNAME" />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you dont have an invite then create &nbsp;
+            <a href="" className="createNewBtn">
+             
+              new room
+            </a>
+          </span>
         </div>
       </div>
+      <footer>
+        <h4>
+            Built By Gautam
+        </h4>
+      </footer>
     </div>
   );
 };

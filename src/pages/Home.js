f23aba26d4f813429a7e6b19e13fd10cd7ad62a4
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-17 16:47:16 +0530

    without css struct of home page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <div>Home</div>
-   )
- }
- 
- export default Home
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img src="/code-sync.png" alt="hi" />
+        <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
+        <div className="inputGroup">
+            <input type="text" className = "inputbox" placeholder = "ROOM ID" />
+            <input type="text" className = "inputbox" placeholder = "USERNAME" />
+            <button className="btn joinBtn">Join</button>
+            <span className="createInfo">
+                If you dont have an invite then create &nbsp;
+                <a href="" className="createNewBtn"> new room</a>
+            </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

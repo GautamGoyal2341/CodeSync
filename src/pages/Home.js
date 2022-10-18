5bbee25f421cd52b886f0c5451641548087ae511
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-18 18:19:38 +0530

    new room unq id set

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,45 @@
 import React from "react";
-import {v4 as uuidV4} from 'uuid'
+import { v4 as uuidV4 } from "uuid";
 const Home = () => {
-    const createNewRoom = (e) => {
-        e.preventDefault();
-        const id = uuidV4();
+  const [roomId, setRommId] = React.useState("");
+  const [username, setUserName] = React.useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
 
-    }
+    const id = uuidV4();
+    setRommId(id);
+  };
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
-        <img className = "homePageLogo" src="/code-sync.png" alt="hi" />
+        <img className="homePageLogo" src="/code-sync.png" alt="hi" />
         <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input type="text" className="inputbox" placeholder="ROOM ID" />
-          <input type="text" className="inputbox" placeholder="USERNAME" />
+          <input
+            type="text"
+            className="inputbox"
+            placeholder="ROOM ID"
+            value={roomId}
+            onChange={(e) => setRommId(e.target.value)}
+          />
+          <input
+            type="text"
+            className="inputbox"
+            placeholder="USERNAME"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
           <button className="btn joinBtn">Join</button>
           <span className="createInfo">
             If you dont have an invite then create &nbsp;
-            <a onClick={createNewRoom} href="" className="createNewBtn">
-             
+            <a  onClick={createNewRoom} href="" className="createNewBtn">
               new room
             </a>
           </span>
         </div>
       </div>
       <footer>
-        <h4>
-            Built By Gautam
-        </h4>
+        <h4>Built By Gautam</h4>
       </footer>
     </div>
   );

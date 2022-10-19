067465ca53cad906ac960e073cd78dd58e06b508
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-19 19:33:09 +0530

    vh css finall done :/

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,35 +1,30 @@
 import React from "react";
 import Client from "../Components/Client";
+import editor from "../Components/editor";
 const EditorPage = () => {
-    const [clients , setClients] = React.useState([
-        {socketId:1 , username : 'Rakesh k '},
-        {socketId:2 , username : 'RJo R '}
-
-    
-    ]);
+  const [clients, setClients] = React.useState([
+    { socketId: 1, username: "Rakesh k " },
+    { socketId: 2, username: "RJo R " },
+  ]);
   return (
     <div className="mainWrap">
       <div className="aside">
         <div className="asideInner">
           <div className="logo">
-            <img src="/code-sync.png" className="logoImage" alt="logo" />
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
           </div>
           <h3>Connected</h3>
           <div className="clientsList">
-            {
-                clients.map((clients) => (
-                    <Client key={clients.socketId} username={clients.username}/>
-                ))
-            }
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
           </div>
         </div>
-        <button className="btn copyBtn">Copy Room</button>
-
+        <button className="btn copyBtn">Copy ROOM ID</button>
         <button className="btn leaveBtn">Leave</button>
-
       </div>
-      <div className="editorwrap">
-        <Editor/>
+      <div className="editorWrap">
+        <editor />
       </div>
     </div>
   );

a79d99565c9b765110e34980d7378c5da4766399
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-19 18:52:15 +0530

    client compo added

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import Client from "../Components/Client";
 const EditorPage = () => {
-  return (
-    <div>EditorPage</div>
-  )
-}
+    const [clients , setClients] = React.useState([
+        {socketId:1 , username : 'Rakesh k '},
+        {socketId:2 , username : 'RJo R '}
 
-export default EditorPage
+    
+    ]);
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img src="/code-sync.png" className="logoImage" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {
+                clients.map((clients) => (
+                    <Client key={clients.socketId} username={clients.username}/>
+                ))
+            }
+          </div>
+        </div>
+      </div>
+      <div className="editorwrap"></div>
+    </div>
+  );
+};
+
+export default EditorPage;

cbfbfb05058e63d8abf5fa347d7e724c089f96df
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-22 12:26:47 +0530

    web socket instance created

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app =express();
+const http = require('http');
+const {Server} = require('socket.io');
+const { Socket } = require('socket.io-client');
+
+
+
+
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('scoket connected' , socket.id);
+})
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT , () => {
+    console.log(`Listening on PORT ${PORT}`)
+})

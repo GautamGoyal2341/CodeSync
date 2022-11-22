5bfc79dcbb2e42e5ace7d9d1848f4356693f0e96
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-22 17:43:27 +0530

    user list from socket id:

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app =express();
 const http = require('http');
 const {Server} = require('socket.io');
 const { Socket } = require('socket.io-client');
+const ACTIONS = require('./src/Actions');
 
 
 
@@ -12,8 +13,31 @@ const { Socket } = require('socket.io-client');
 const server = http.createServer(app);
 const io = new Server(server);
 
+
+const userSocketMap = {};
+function getAllConnectedClients(roomId)
+{
+  return     Array.from(io.sockets.adapter.getMaxListeners(roomId) || []).map((socketId) => {
+              return {
+                socketId,
+                username: userSocketMap[socketId]
+              }
+  })
+}
+
+
 io.on('connection', (socket) => {
     console.log('scoket connected' , socket.id);
+
+
+    socket.on(ACTIONS.JOIN , ({roomId,username})=>{
+
+        userSocketMap[socket.id] = username;
+        socket.join(roomId); 
+        const clients = getAllConnectedClients(roomId);
+
+    });
+
 })
 
 const PORT = process.env.PORT || 5000;
